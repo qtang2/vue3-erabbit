1eb3489bc8538ee728f8677d3d7a11ed7f953e50
Qian Tang <q.tang6@student.unimelb.edu.au>
2021-09-29 10:49:26 +1000

    finish head and footer layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-  <div class="class">
-    APP
-    <button @click="fn">test request</button>
+  <div class="app">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import request from '@/utils/request'
 export default {
-  setup () {
-    const fn = () => {
-      request('/member/profile', 'get', { a: 10 })
-    }
-
-    return { fn }
-  }
+  name: 'App'
 }
 </script>
 
---
+++ src/views/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <app-topnav />
+  <app-header />
+  <main class="app-body">
+    <!-- 二级路由 -->
+    <router-view></router-view>
+  </main>
+  <app-footer />
+</template>
+
+<script>
+import AppTopnav from '@/components/app-topnav.vue'
+import AppHeader from '@/components/app-header.vue'
+import AppFooter from '@/components/app-footer.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    AppTopnav,
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.app-body {
+  min-height: 600px;
+}
+</style>
